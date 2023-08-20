293ef6e5da450f1c48d9f4523b169a5da20158de
Danylo Sus <danil.sushko19@gmail.com>
2023-08-20 09:38:17 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fylo data storage component master
+
+from *www.frontendmentor.io*
+
+- [ ] Desktop Design
+- [ ] Responsive Design
+- [ ] Animations
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss')",.Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "pale-blue": "hsl(243, 100%, 93%)",
+        "grayish-blue": "hsl(229, 7%, 55%)",
+        "dark-blue": "hsl(228, 56%, 26%)",
+        "very-dark-blue": "hsl(229, 57%, 11%)",
+
+        "gradient-from": "hsl(6, 100%, 80%)",
+        "gradient-to": "hsl(335, 100%, 65%)",
+      },
+    },
+  },
+  plugins: [],
+};
